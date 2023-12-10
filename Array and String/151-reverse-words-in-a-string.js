cade3d2b17cff017a2edde29b0294d2c6ba15000
rrupom <rakib.t.rupom@gmail.com>
2023-12-10 11:39:16 +0600

    feat:update

--- Array and String/151-reverse-words-in-a-string.js
+++
@@ -1,8 +0,0 @@
-var reverseWords = function (s) {
-  let words = s.trim().split(" ");
-  words = words.filter((word) => word != "");
-  return words.reverse().join(" ");
-};
-
-let s = "the sky is blue";
-console.log(reverseWords(s));
--- Array and String/58-length-of-last-word.js
+++
@@ -1,7 +0,0 @@
-var lengthOfLastWord = function (s) {
-  let array = s.trim(" ").split(" ");
-  let arr = array.filter((element) => element != "");
-  return arr[arr.length - 1].length;
-};
-
-console.log(lengthOfLastWord("Hello World"));
